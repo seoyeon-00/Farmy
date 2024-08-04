244bdf21e805ac46416ec048435ca6c20ceb3d88
ksy <ksoup3434@gmail.com>
2024-08-04 22:15:04 +0900

    Feat: 헤더 작업중

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,31 @@
+import { Styled } from 'styled-components';
+import logo from '../../assets/common/logo.png';
+
 const Header = () => {
-  return <div>헤더</div>;
+  return (
+    <header>
+      <h1>
+        <img src={logo} alt='로고' />
+      </h1>
+      <ul>
+        <li>
+          <a>파미 소개</a>
+        </li>
+        <li>
+          <a>농업기술 정보</a>
+        </li>
+        <li>
+          <a>농업인 교육</a>
+        </li>
+        <li>
+          <a>팜로그</a>
+        </li>
+        <li>
+          <a>커뮤니티</a>
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
