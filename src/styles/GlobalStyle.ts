348fe2d340dd6a4c7acdb4118ada9885f5c4735d
ksy <ksoup3434@gmail.com>
2024-08-11 19:38:29 +0900

    Feat: 전역, 레이아웃 스타일 설정

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,8 +1,40 @@
 import { createGlobalStyle } from 'styled-components';
+import layout from './LayoutGuide';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset};
+  ${layout};
+
+  a {
+    color: #333;
+    text-decoration: none;
+  }
+
+  :root {
+    // Colors
+    --color-main: #F86800;
+    --color-sub: #FFBC8C;
+    --color-pastel: #FFEDE0;
+    --color-font-main: #333;
+    --color-gray: #FCFCFC;
+
+    // Font size
+    --font-title: clamp(3rem, 5.5vw, 4rem);
+    --font-sub-title: clamp(2.2rem, 4.17vw, 3rem);
+    --font-body-large: clamp(2rem, 3.34vw, 2.4rem);
+    --font-body-medium: clamp(1.8rem, 2.78vw, 2rem);
+    --font-body-regular: clamp(1.4rem, 2.23vw, 1.6rem);
+    --font-body-small: clamp(1.3rem, 1.94vw, 1.4rem);
+    --font-body-xsmall: clamp(1.1rem, 1.8vw, 1.3rem);
+    --font-body-micro: clamp(1rem, 1.67vw 1.2rem);
+
+    // Font weight
+    --weight-bold: 900;
+    --weight-semi-bold: 700;
+    --weight-regular: 500;
+    --weight-light: 300;
+  }
 `;
 
 export default GlobalStyle;
---
+++ src/styles/LayoutGuide.ts
@@ -0,0 +1,62 @@
+import { css } from 'styled-components';
+
+const LayoutGuide = css`
+  .wrap {
+    max-width: 1500px;
+    margin: 0 auto;
+  }
+
+  .flex {
+    display: flex;
+
+    &_col {
+      flex-direction: column;
+    }
+
+    &_cc {
+      justify-content: center;
+      align-items: center;
+    }
+
+    &_cs {
+      justify-content: center;
+      align-items: flex-start;
+    }
+
+    &_ce {
+      justify-content: center;
+      align-items: flex-end;
+    }
+
+    &_bs {
+      justify-content: space-between;
+      align-items: flex-start;
+    }
+
+    &_bc {
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    &_ac {
+      justify-content: space-around;
+      align-items: center;
+    }
+
+    &_sc {
+      justify-content: flex-start;
+      align-items: center;
+    }
+
+    &_ec {
+      justify-content: flex-end;
+      align-items: center;
+    }
+  }
+
+  .grid {
+    display: grid;
+  }
+`;
+
+export default LayoutGuide;
